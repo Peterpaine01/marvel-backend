1726595b600461a7a26a5c894f5ea30d3d795196
Fanny Carlier <fannycarlier@mbp-de-fanny.home>
2023-11-16 18:22:30 +0100

    search

--- routes/characters.js
+++ routes/characters.js
@@ -7,12 +7,20 @@ const axios = require("axios");
 
 // ROUTE GET > list of all characters
 router.get("/characters", async (req, res) => {
-  const response = await axios.get(
-    `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MY_API_KEY}`
-  );
-  res.json(response.data);
+  try {
+    const { name, skip } = req.query.name;
+
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MY_API_KEY}&name=${name}&skip=${skip}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
+module.exports = router;
 // ROUTE GET > infos of a specific character
 router.get("/character/:characterId", async (req, res) => {
   const response = await axios.get(
@@ -20,5 +28,3 @@ router.get("/character/:characterId", async (req, res) => {
   );
   res.json(response.data);
 });
-
-module.exports = router;
